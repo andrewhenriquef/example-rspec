27977767f6bcf81ee924e3f05ca2836a25a67c83
Andrew Oliveira <andrewhenriquef@gmail.com>
2018-03-23 07:12:26 -0300

    Created app to learn how to create tests, described a class calculater with context to sum

---
+++ spec/calculator/calculator_spec.rb
@@ -0,0 +1,22 @@
+require 'calculator'
+
+describe Calculator do
+  context '#sum' do
+    subject(:calc) { described_class.new }
+
+    it 'with positive numbers' do
+      result = calc.sum(5, 7)
+      expect(result).to equal(12)
+    end
+
+    it 'with negative and positive numbers' do
+      result = calc.sum(-5, 7)
+      expect(result).to equal(2)
+    end
+
+    it 'with negative numbers' do
+      result = calc.sum(-5, -7)
+      expect(result).to equal(-12)
+    end
+  end
+end
