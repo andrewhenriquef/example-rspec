a2fb938837cc582e9bc1a6deea58d72764781c03
Andrew Oliveira <andrewhenriquef@gmail.com>
2018-03-26 07:24:31 -0300

    Matchers for equality and truthiness

---
+++ spec/matchers/comparacao/comparacao_spec.rb
@@ -0,0 +1,29 @@
+describe 'Matchers de comparação' do
+  
+  it '#equal testa se é o mesmo objeto' do
+    x = "Ruby"
+    y = "Ruby"
+    # equal test if the objects are equals
+    expect(x).to_not equal(y)
+  end
+  
+  it '#be também testa se é o mesmo objeto' do
+    x = "Ruby"
+    y = "Ruby"
+    # 'be' does exactly same thing
+    expect(x).to_not be(y)
+  end
+  
+  it '#eq testa se é o mesmo valor' do
+    x = "Ruby"
+    y = "Ruby"
+    # 'be' does exactly same thing
+    expect(x).to eq(y)
+  end
+  it '#eql também testa se é o mesmo valor' do
+    x = "Ruby"
+    y = "Ruby"
+    # 'be' does exactly same thing
+    expect(x).to eql(y)
+  end
+end
