ee5c542fea8c53c6b63010b10627d6c5549f51b1
Andrew Oliveira <andrewhenriquef@gmail.com>
2019-03-06 21:40:19 -0300

    (=) Change hooks to around(around execute before and after code)

--- spec/matchers/atributos/atributos_spec.rb
+++ spec/matchers/atributos/atributos_spec.rb
@@ -1,12 +1,16 @@
 require 'pessoa'
 
 describe 'Atributos' do
+  around(:each) do |teste|
+    puts 'Antes'
+    @pessoa = Pessoa.new
+    teste.run # roda o teste
+    puts "Depois >>>> #{@pessoa}"
+  end
+
   it 'have_attributes' do
-    pessoa = Pessoa.new
-
-    pessoa.name = 'Andrew'
-    pessoa.age = 23
-
-    expect(pessoa).to have_attributes(name: start_with('A'), age: (be >= 20))
+    @pessoa.name = 'Andrew'
+    @pessoa.age = 23
+    expect(@pessoa).to have_attributes(name: start_with('A'), age: (be >= 20))
   end
 end
