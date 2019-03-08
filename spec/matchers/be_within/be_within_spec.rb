de35ca69141de8f08bffa0d3e4b96f40e3ad81b7
Andrew Oliveira <andrewhenriquef@gmail.com>
2019-03-07 22:33:13 -0300

    (+) Define negative matcher

--- spec/matchers/be_within/be_within_spec.rb
+++ spec/matchers/be_within/be_within_spec.rb
@@ -1,5 +1,9 @@
+RSpec::Matchers.define_negated_matcher :be_not_within, :be_within
+
 describe 'be_within' do
   # intervalo entre um valor (pode ter uma margem de 0.5 para cima ou para baixo)
   it { expect(12.5).to be_within(0.5).of(12) }
   it { expect([11.6, 12.1, 12.4]).to all be_within(0.5).of(12) }
+
+  it { expect(12.6).to be_not_within(0.5).of(12) }
 end
