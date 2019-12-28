4a2586c4bc73a95a1a5b113fd76b79c77414a49f
Andrew Oliveira <andrewhenriquef@gmail.com>
2019-12-28 16:20:06 -0300

    (+) Add as null object example

--- spec/teste_doubles/user_spec.rb
+++ spec/teste_doubles/user_spec.rb
@@ -10,4 +10,18 @@ describe 'Test Double' do
     user.name
     user.password
   end
+
+  it 'as_null_object' do
+    # fazer a chamada e dispensar a mensagem, transforma em um objeto nulo
+    # some com as mensagens de erro do double
+    user = double('User').as_null_object
+
+    allow(user).to receive(:name).and_return('Jack')
+    allow(user).to receive(:password).and_return('secret')
+
+    puts user.name
+    puts user.password
+
+    user.abc
+  end
 end
